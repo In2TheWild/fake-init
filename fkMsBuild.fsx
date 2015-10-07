d72a9ae4ac72b8f0fca0e6803a8dfee1afce693b
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-10-07 17:25:48 +0700

    test restore packages

--- fkMsBuild.fsx
+++ fkMsBuild.fsx
@@ -5,6 +5,19 @@ open Fake
 open Fake.FileSystemHelper
 open Fake.MSBuildHelper
 open System
+open System.IO
+
+//let sln = @"y:/Source/project/tourism/tourism-report-client/ReportClient.sln"
+let sln = @"y:/Source/fake/tourism-web/TourismWeb.sln"
+let info = FileInfo(sln)
+
+Target "restore" (fun _ ->
+    sln
+    |> RestoreMSSolutionPackages (fun p ->
+         { p with
+             OutputPath = Path.Combine(info.Directory.FullName, "packages")
+             Retries = 4 })
+ )
 
 let buildMode = getBuildParamOrDefault "buildMode" "Release"
 let setParams defaults =
@@ -18,10 +31,9 @@ let setParams defaults =
                     "Configuration", buildMode
                 ]
          }
-let sln = @"y:/Source/project/tourism/tourism-report-client/ReportClient.sln"
 
 Target "build" (fun _ ->
         build setParams sln |> DoNothing
     )
 
-RunTargetOrDefault "build"
+RunTargetOrDefault fsi.CommandLineArgs.[1]
